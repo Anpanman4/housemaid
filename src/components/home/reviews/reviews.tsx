4876334f956b057d2d8713dd8852f6228ba8ff20
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-08-31 16:51:43 +0300

    feat: functional for rewiews buttons

--- src/components/home/reviews/reviews.tsx
+++ src/components/home/reviews/reviews.tsx
@@ -8,20 +8,61 @@ import { useState } from "react";
 
 const Reviews = () => {
   const [currentCards, setCurrentCards] = useState({ first: 0, second: 1 });
+  const [isVisibleLeftArrow, setIsVisibleLeftArrow] = useState(false);
+  const [isVisibleRightArrow, setIsVisibleRightArrow] = useState(true);
 
-  const moveReview = () => {
-    if (currentCards.first === peoples.length) return setCurrentCards({ first: 0, second: peoples.length });
-    if (currentCards.second === peoples.length) return setCurrentCards({ first: 1, second: 0 });
-    return setCurrentCards({ first: currentCards.first + 1, second: currentCards.second + 1 });
+  const checkArrowVisible = (mark: string) => {
+    if (mark === "+") {
+      setIsVisibleLeftArrow(true);
+      if (currentCards.second === peoples.length - 2) setIsVisibleRightArrow(false);
+    } else {
+      setIsVisibleRightArrow(true);
+      if (currentCards.first === 1) setIsVisibleLeftArrow(false);
+    }
+  };
+
+  const moveReview = (mark: string) => {
+    if (mark === "+") {
+      setCurrentCards({ first: currentCards.first + 1, second: currentCards.second + 1 });
+      checkArrowVisible("+");
+    } else {
+      setCurrentCards({ first: currentCards.first - 1, second: currentCards.second - 1 });
+      checkArrowVisible("-");
+    }
   };
 
   return (
     <section className={style.reviews}>
       <h2 className={style.reviews__title}>Reviews</h2>
       <div className={style.reviews__container}>
+        {isVisibleLeftArrow ? (
+          <Image
+            className={`${style.reviews__arrow} ${style.reviews__arrow_left}`}
+            onClick={() => moveReview("-")}
+            src={arrow}
+            alt="Next"
+            priority={true}
+            width="0"
+            height="0"
+          ></Image>
+        ) : (
+          ""
+        )}
         <Card people={peoples[currentCards.first]}></Card>
         <Card people={peoples[currentCards.second]}></Card>
-        <Image className={style.reviews__arrow} onClick={moveReview} src={arrow} alt="Next" priority={true}></Image>
+        {isVisibleRightArrow ? (
+          <Image
+            className={`${style.reviews__arrow} ${style.reviews__arrow_right}`}
+            onClick={() => moveReview("+")}
+            src={arrow}
+            alt="Next"
+            priority={true}
+            width="0"
+            height="0"
+          ></Image>
+        ) : (
+          ""
+        )}
       </div>
     </section>
   );
--- src/components/home/reviews/utils.js
+++ src/components/home/reviews/utils.js
@@ -15,12 +15,6 @@ const peoples = [
     text: `I love the feeling of a clean and organized space, but I don't always have the motivation to clean. This service helped me.`,
   },
   {
-    name: "Melissa",
-    stars: 5,
-    src: melissa,
-    text: `I used to hate cleaning, but now I find it very therapeutic. There's something satisfying about having a clean and organized space`,
-  },
-  {
     name: "John",
     stars: 5,
     src: john,
@@ -31,12 +25,6 @@ const peoples = [
     stars: 5,
     src: melissa,
     text: `I used to hate cleaning, but now I find it very therapeutic. There's something satisfying about having a clean and organized space`,
-  },
-  {
-    name: "John",
-    stars: 5,
-    src: john,
-    text: `I love the feeling of a clean and organized space, but I don't always have the motivation to clean. This service helped me.`,
   },
   {
     name: "Melissa",
